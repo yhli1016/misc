30b13633af911b0038285d866f9611568e70f0bc
yhli <liyunhai1016@hotmail.com>
2018-07-16 11:28:22 +0800

    Added developer tools from Quantum ESPRESSO

---
+++ devtools/makedeps.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+# compute dependencies for the PWscf directory tree
+
+# make sure there is no locale setting creating unneeded differences.
+LC_ALL=C
+export LC_ALL
+
+# run from directory where this script is
+echo "The script for adding new dependencies is running"
+echo "Usage: $0 DIR DEPENDENCY_DIRS"
+echo "dependencies in DEPENDENCY_DIRS will be searched for DIR"
+shift
+DEPENDS=$*
+
+# generate dependencies file (only for directories that are present)
+moduledep.sh $DEPENDS > make.depend
+includedep.sh $DEPENDS >> make.depend
+
+# handle special cases: modules for C-fortran binding, hdf5, MPI
+sed '/@mpi@/d' make.depend > make.depend.tmp
+mv make.depend.tmp make.depend
+
+# check for missing dependencies 
+if grep @ make.depend
+then
+   notfound=1
+   echo WARNING: dependencies not found in directory $DIR
+else
+    echo directory $DIR : ok
+fi
+
+if test "$notfound" = ""
+then
+    echo all dependencies updated successfully
+fi
---
+++ templates/bgw_dft/run_wfn.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+topdir=$(pwd)
+launcher="mpirun -np 20"
+
+cd $topdir/01-scf
+$launcher pw.x -in scf.in &> scf.out
+wait
+
+for pref in 02-wfn 03-wfnq 04-wfn_fi; do
+    cd $topdir/$pref
+    cp -r ../01-scf/*.save .
+    wait
+    $launcher pw.x -in bands.in &> bands.out
+    wait
+    $launcher pw2bgw.x -in p2b.in &> p2b.out
+    wait
+    rm -rf *.save *wfc* *igk*
+    wait
+done
