bf3c2559463926d0396836298ac14efa24f691b2
yhli <liyunhai1016@hotmail.com>
2020-09-09 17:56:35 +0800

    Rewritten engwin.py for wannier90

--- wannier90/engwin.py
+++ wannier90/engwin.py
@@ -1,48 +1,53 @@
 #! /usr/bin/env python
-
-# Wannier90 requires the outer and frozen energy windows in the input if
-# disentanglement is enabled. This program extracts the energy window for
-# given band from the output of pw.x.
-#
-# Usage: engwin.py bands.out iband, where iband is counting from 1
-
 import sys
-
-pwoutfn = sys.argv[1]
-iband = int(sys.argv[2])
-
-pwoutfile = open(pwoutfn, "r")
-pwout = pwoutfile.readlines()
-pwoutfile.close()
-
-# extract line numbers of kpoints
-nlindex = []
-for line in pwout:
-    if line.find("k =") != -1 and line.find("bands") != -1:
-        nlindex.append(pwout.index(line))
+import xml.dom.minidom
 
 
-# extract full band structure
-band = []
-# loop over kpoints
-for nl in nlindex:
-    band_k = []
-    # loop over bands
-    for line in pwout[nl+2:]:
-        sline = line.split()
-        # check if this line contains data
-        if len(sline) > 0:
-            for eng in sline:
-                band_k.append(float(eng))
-        else:
-            break
-    # add band_k to band
-    band.append(band_k)
+Har2eV = 13.60569253 * 2
 
-# extract band structure for given band
-band_i = []
-for band_k in band:
-    band_i.append(band_k[iband-1])
 
-# echo
-print min(band_i), max(band_i)
+def get_energies(xml_name):
+    dom = xml.dom.minidom.parse(xml_name)
+    root = dom.documentElement
+    eng_full = []
+    if root.nodeName == "fleurOutput":
+        eigenvalues = root.getElementsByTagName("eigenvalues")[-1]
+        eks = eigenvalues.getElementsByTagName("eigenvaluesAt")
+        eng_full = [[float(f) for f in ek.childNodes[0].data.split()] for ek in eks]
+    elif root.nodeName == "qes:espresso":
+        eigenvalues = root.getElementsByTagName("eigenvalues")
+        eng_full = [[float(f) for f in ek.childNodes[0].data.split()] for ek in eigenvalues]
+    else:
+        raise RuntimeError("Unknown xml output")
+    return eng_full
+
+
+def main():
+    xml_name = sys.argv[1]
+    option = sys.argv[2]
+    if option == "e":
+        # Band index is counted from 1, NOT 0.
+        band_index = int(sys.argv[3])
+        eng_full = get_energies(xml_name)
+        eng_selected =[eng[band_index-1] for eng in eng_full]
+        emin = min(eng_selected) * Har2eV
+        emax = max(eng_selected) * Har2eV
+        print("emin = %f" % emin)
+        print("emax = %f" % emax)
+    elif option == "n":
+        # Energies are in eV, not Hartree.
+        emin = float(sys.argv[3]) / Har2eV
+        emax = float(sys.argv[4]) / Har2eV
+        eng_full = get_energies(xml_name)
+        for ik, ek in enumerate(eng_full):
+            num_bands = 0
+            for eng in ek:
+                if eng >= emin and eng <= emax:
+                    num_bands += 1
+            print("ik = %d, nbnd = %d" % (ik+1, num_bands))
+    else:
+        print("ERROR: unknown option '%s'" % option)
+
+
+if __name__ == "__main__":
+    main()
