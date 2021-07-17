bc96db4d3a9c2f61ba829d275f6788ca6909adc3
yhli <liyunhai1016@hotmail.com>
2021-07-17 12:39:55 +0800

    Added fine control over printing style of print_banner

--- linux/print_banner.py
+++ linux/print_banner.py
@@ -1,28 +1,26 @@
 #! /usr/bin/env python
 
 
-def print_banner_line(banner=None, width=80, mark="-"):
+def print_banner_line(text, width=80, mark="-", end="#"):
     """
-    Print a banner like '#--------------- FOO ---------------' to stdout.
+    Print a banner like '#--------------- FOO ---------------#' to stdout.
 
-    The magic number 3 accounts for a '#' ahead of the banner and two spaces
-    wrapping the banner text.
-
-    :param banner: string, central text in the banner
+    :param text: string, central text in the banner
     :param width: integer, total width of the banner
     :param mark: character, marker
+    :param end: character, end prepended and appended to the banner
     :return: None
     """
-    num_marks_total = width - len(banner) - 3
+    num_marks_total = width - len(text) - 4
     num_marks_left = num_marks_total // 2
     num_marks_right = num_marks_total - num_marks_left
-    banner_with_marks = "#" + "".join([mark for _ in range(num_marks_left)])
-    banner_with_marks += " %s " % banner
-    banner_with_marks += "".join([mark for _ in range(num_marks_right)])
+    banner_with_marks = end + mark * num_marks_left
+    banner_with_marks += " %s " % text
+    banner_with_marks += mark * num_marks_right + end
     print(banner_with_marks)
 
 
-def print_banner_block(banner, width=80, mark="-"):
+def print_banner_block(text, width=80, mark="-", end="#"):
     """
     Print a banner like
     #----------------------------------#
@@ -30,18 +28,19 @@ def print_banner_block(banner, width=80, mark="-"):
     #----------------------------------#
     to stdout.
 
-    :param banner: string, central text in the banner
+    :param text: string, central text in the banner
     :param width: integer, total width of the banner
     :param mark: character, marker
+    :param end: character, end prepended and appended to the banner
     :return: None
     """
-    num_spaces_total = width - len(banner) - 2
+    num_spaces_total = width - len(text) - 2
     num_spaces_left = num_spaces_total // 2
     num_spaces_right = num_spaces_total - num_spaces_left
-    banner_with_spaces = "#" + "".join([" " for _ in range(num_spaces_left)])
-    banner_with_spaces += banner
-    banner_with_spaces += "".join([" " for _ in range(num_spaces_right)]) + "#"
-    border = "#" + "".join([mark for _ in range(width-2)]) + "#"
+    banner_with_spaces = end + " " * num_spaces_left
+    banner_with_spaces += text
+    banner_with_spaces += " " * num_spaces_right + end
+    border = end + mark * (width - 2) + end
     print(border)
     print(banner_with_spaces)
     print(border)
@@ -50,13 +49,15 @@ def print_banner_block(banner, width=80, mark="-"):
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("banner", type=str,  action="store")
+    parser.add_argument("text", type=str,  action="store")
     parser.add_argument("--width", "-w",   type=int, action="store", default="80")
     parser.add_argument("--type", "-t", type=str, action="store", default="line")
+    parser.add_argument("--mark", "-m", type=str, action="store", default="-")
+    parser.add_argument("--end", "-e", type=str, action="store", default="#")
     args = parser.parse_args()
     if args.type == "line":
-        print_banner_line(args.banner, args.width)
+        print_banner_line(args.text, args.width, args.mark, args.end)
     elif args.type == "block":
-        print_banner_block(args.banner, args.width)
+        print_banner_block(args.text, args.width, args.mark, args.end)
     else:
         raise ValueError("ERROR: Unknown banner type '%s'" % args.flavor)
