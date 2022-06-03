960954cd0d678a3dd360b77d650bc54a624279ed
Yunhai Li <liyunhai1016@hotmail.com>
2022-06-03 17:19:21 +0800

    Rewrite idpp

--- physics/nebtools/idpp.py
+++ physics/nebtools/idpp.py
@@ -2,7 +2,6 @@
 """Prepare directories and POSCARs for NEB Calculation."""
 
 import os
-import math
 
 import numpy as np
 from scipy.optimize import minimize
@@ -10,99 +9,15 @@ from ase.io import read, write
 from ase.neb import NEB
 
 
-def vmod(x):
+def norm(x):
     """
-    Calculate vector modulus.
+    Calculate the norm of vector.
 
     :param np.ndarray x: incoming vector
     :return: modulus of x
     :rtype: float
     """
-    return math.sqrt(np.sum(x**2).item())
-
-
-def correct_pbc(ref_image, chk_image, selected_atoms="all"):
-    """
-    Correct the coordinates of atoms wrapped back by periodic boundary
-    condition to minimize the mismatch.
-
-    :param ase.Atoms ref_image: reference image
-    :param ase.Atoms chk_image: image to correct
-    :param List[int] selected_atoms: indices of atoms taken into consideration
-    :return: None. chk_image is modified.
-    """
-    # Extract lattice vectors and atomic positions
-    ref_cell = ref_image.cell
-    ref_pos = ref_image.get_positions()
-    chk_pos = chk_image.get_positions()
-    diff_pos = ref_pos - chk_pos
-
-    # Select all atoms if needed
-    if selected_atoms == "all":
-        selected_atoms = select_atoms(ref_image, exclude_atoms=None, offset=0)
-
-    # Check and fix atoms wrapped back by periodic boundary condition
-    neighbor_r0 = [(i, j, k)
-                   for i in range(-1, 2)
-                   for j in range(-1, 2)
-                   for k in range(-1, 2)]
-    neighbor_r0 = ref_cell.cartesian_positions(neighbor_r0)
-    for i_a in selected_atoms:
-        dist = [vmod(diff_pos[i_a] - r0) for r0 in neighbor_r0]
-        chk_pos[i_a] += neighbor_r0[dist.index(min(dist))]
-    chk_image.set_positions(chk_pos)
-
-
-def align_image(ref_image, chk_image, selected_atoms="all"):
-    """
-    Align image to reference to minimize the mismatch.
-
-    :param ase.Atoms ref_image: reference image
-    :param ase.Atoms chk_image: image to align
-    :param List[int] selected_atoms: indices of atoms taken into consideration
-    :return: None. chk_image is modified.
-    """
-    # Extract lattice vectors and atomic positions
-    ref_pos = ref_image.get_positions()
-    chk_pos = chk_image.get_positions()
-    diff_pos = ref_pos - chk_pos
-
-    # Select all atoms if needed
-    if selected_atoms == "all":
-        selected_atoms = select_atoms(ref_image, exclude_atoms=None, offset=0)
-
-    # Find the minimal displacement vector
-    def _min_obj(x):
-        return sum([vmod(diff_pos[i_a] - x)**2 for i_a in selected_atoms])
-
-    x0 = np.sum(diff_pos[selected_atoms], axis=0) / len(selected_atoms)
-    result = minimize(_min_obj, x0)
-    print(result)
-
-    # Align the image according to displacement vector
-    if result.status == 0:
-        chk_pos += result.x
-    else:
-        chk_pos += x0
-    chk_image.set_positions(chk_pos)
-
-
-def center_image(image, center=0.5, selected_atoms="all"):
-    """
-    Center the image at given position along z-axis.
-
-    :param ase.Atoms image: image to center
-    :param float center: position to center the image
-    :param List[int] selected_atoms: indices of atoms taken into consideration
-    :return: None. Incoming image is modified.
-    """
-    if selected_atoms == "all":
-        selected_atoms = select_atoms(image, exclude_atoms=None, offset=0)
-    scaled_pos = image.get_scaled_positions()
-    old_center = np.sum(scaled_pos[selected_atoms], axis=0)
-    old_center /= len(selected_atoms)
-    scaled_pos[:, 2] += (center - old_center[2])
-    image.set_scaled_positions(scaled_pos)
+    return np.linalg.norm(x).item()
 
 
 def select_atoms(image, exclude_atoms=None, offset=0):
@@ -122,17 +37,95 @@ def select_atoms(image, exclude_atoms=None, offset=0):
     return selected_atoms
 
 
-def interpolate(initial_image, final_image, num_inter_images, interp="idpp",
-                mic=True):
+def correct_pbc(ref_image, chk_image, **kwargs):
+    """
+    Correct the coordinates of atoms wrapped back by periodic boundary
+    condition to minimize the mismatch.
+
+    :param ase.Atoms ref_image: reference image
+    :param ase.Atoms chk_image: image to correct
+    :param kwargs: arguments for 'select_atoms'
+    :return: None. chk_image is modified.
+    """
+    # Extract lattice vectors and atomic positions
+    ref_cell = ref_image.cell
+    ref_pos = ref_image.get_positions()
+    chk_pos = chk_image.get_positions()
+    diff_pos = ref_pos - chk_pos
+
+    # Select atoms
+    selected_atoms = select_atoms(ref_image, **kwargs)
+
+    # Check and fix atoms wrapped back by periodic boundary condition
+    neighbor_r0 = [(i, j, k)
+                   for i in range(-1, 2)
+                   for j in range(-1, 2)
+                   for k in range(-1, 2)]
+    neighbor_r0 = ref_cell.cartesian_positions(neighbor_r0)
+    for i_a in selected_atoms:
+        dist = [norm(diff_pos[i_a] - r0) for r0 in neighbor_r0]
+        chk_pos[i_a] += neighbor_r0[dist.index(min(dist))]
+    chk_image.set_positions(chk_pos)
+
+
+def align_image(ref_image, chk_image, **kwargs):
+    """
+    Align image to reference to minimize the mismatch.
+
+    :param ase.Atoms ref_image: reference image
+    :param ase.Atoms chk_image: image to align
+    :param kwargs: arguments for 'select_atoms'
+    :return: None. chk_image is modified.
+    """
+    # Extract lattice vectors and atomic positions
+    ref_pos = ref_image.get_positions()
+    chk_pos = chk_image.get_positions()
+    diff_pos = ref_pos - chk_pos
+
+    # Select atoms
+    selected_atoms = select_atoms(ref_image, **kwargs)
+
+    # Find the minimal displacement vector
+    def _min_obj(x):
+        return sum([norm(diff_pos[i_a] - x)**2 for i_a in selected_atoms])
+
+    x0 = np.sum(diff_pos[selected_atoms], axis=0) / len(selected_atoms)
+    result = minimize(_min_obj, x0)
+    print(result)
+
+    # Align the image according to displacement vector
+    if result.status == 0:
+        chk_pos += result.x
+    else:
+        chk_pos += x0
+    chk_image.set_positions(chk_pos)
+
+
+def center_image(image, center=0.5, **kwargs):
+    """
+    Center the image at given position along z-axis.
+
+    :param ase.Atoms image: image to center
+    :param float center: position to center the image
+    :param kwargs: arguments for 'select_atoms'
+    :return: None. Incoming image is modified.
+    """
+    selected_atoms = select_atoms(image, **kwargs)
+    scaled_pos = image.get_scaled_positions()
+    old_center = np.sum(scaled_pos[selected_atoms], axis=0)
+    old_center /= len(selected_atoms)
+    scaled_pos[:, 2] += (center - old_center[2])
+    image.set_scaled_positions(scaled_pos)
+
+
+def interpolate(initial_image, final_image, num_inter_images, **kwargs):
     """
     Generate a path by interpolation between initial and final images.
 
     :param ase.Atoms initial_image: initial state
     :param ase.Atoms final_image: final state
     :param int num_inter_images: number of intermediate states
-    :param str interp: interpolation method
-    :param bool mic: whether to enable minimal image correction for idpp
-        interpolation
+    :param kwargs: arguments for 'neb.interpolate'
     :return: path connecting initial and final images
     :rtype: List[ase.Atoms]
     """
@@ -140,7 +133,7 @@ def interpolate(initial_image, final_image, num_inter_images, interp="idpp",
     images += [initial_image.copy() for _ in range(num_inter_images)]
     images.append(final_image)
     neb = NEB(images)
-    neb.interpolate(method=interp, mic=mic)
+    neb.interpolate(**kwargs)
     return images
 
 
@@ -159,7 +152,7 @@ def main():
     center_z = 0.2
 
     # Interpolation parameters
-    interp = "idpp"
+    method = "idpp"
     mic = True
 
     # Debugging flags
@@ -170,29 +163,29 @@ def main():
     image_fin = read(poscar_fin, index=-1)
 
     # Correct final image
-    fixed_atoms = select_atoms(image_ini, exclude_atoms=free_atoms,
-                               offset=offset)
+    select_args = {"exclude_atoms": free_atoms, "offset": offset}
     if _correct_pbc:
-        correct_pbc(image_ini, image_fin, fixed_atoms)
+        correct_pbc(image_ini, image_fin, **select_args)
     if _align_image:
-        align_image(image_ini, image_fin, fixed_atoms)
+        align_image(image_ini, image_fin, **select_args)
 
     # Center initial and final images
     if _center_image:
-        center_image(image_ini, center_z, fixed_atoms)
-        center_image(image_fin, center_z, fixed_atoms)
+        center_image(image_ini, center_z, **select_args)
+        center_image(image_fin, center_z, **select_args)
 
     # Interpolate
-    mep = interpolate(image_ini, image_fin, num_inter_images, interp, mic)
+    mep = interpolate(image_ini, image_fin, num_inter_images,
+                      method=method, mic=mic)
 
     # Output
     if not debug:
-        vasp_param = {"vasp5": True, "direct": True}
+        vasp_args = {"vasp5": True, "direct": True}
         os.system("rm -rf 0*")
         for i, image in enumerate(mep):
             dir_name = "%02d" % i
             os.mkdir(dir_name)
-            write("%s/POSCAR" % dir_name, image, format="vasp", **vasp_param)
+            write("%s/POSCAR" % dir_name, image, format="vasp", **vasp_args)
     write("mep.traj", mep)
 
 
