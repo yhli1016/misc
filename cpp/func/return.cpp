87fb3f3c77553b7b5a43ecef92ae1918fc462af6
yhli <liyunhai1016@hotmail.com>
2024-02-06 15:11:17 +0800

    Add correct method to return ref and ptr

--- cpp/func/return.cpp
+++ cpp/func/return.cpp
@@ -5,18 +5,34 @@ Base return_val(int i) {
     b.log("return_val");
     return b;
 }
+/*
+Base & return_ref(int i) {
+    Base b(i);
+    b.log("return_ref");
+    return b;
+}
 
-// Base & return_ref(int i) {
-//     Base b(i);
-//     b.log("return_val");
-//     return b;
-// }
-
-// Base * return_ptr(int i) {
-//     Base b(i);
-//     b.log("return_val");
-//     return &b;
-// }
+Base * return_ptr(int i) {
+    Base b(i);
+    b.log("return_ptr");
+    return &b;
+}
+*/
+class TestClass {
+    private:
+        Base b;
+    public:
+        TestClass(int i): b(i){};
+        ~TestClass() {};
+        const Base& return_ref() const {
+            b.log("return_ref");
+            return b;
+        }
+        const Base* return_ptr() const {
+            b.log("return_ptr");
+            return &b;
+        }
+};
 
 int main() {
     cout << "Return by value:" << endl;
@@ -26,12 +42,16 @@ int main() {
 
     cout << "Return by reference:" << endl;
     //Base &b2 = return_ref(3);
-    //b2.log("access_ref");
+    TestClass t2(3);
+    const Base &b2 = t2.return_ref();
+    b2.log("access_ref");
     cout << endl;
 
     cout << "Return by pointer:" << endl;
     //Base *pb3 = return_ptr(4);
-    //pb3->log("access_ptr");
+    TestClass t3(4);
+    const Base *pb3 = t3.return_ptr();
+    pb3->log("access_ptr");
     cout << endl;
 
     cout << "Destruction:" << endl;
