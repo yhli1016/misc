17093cd44cda5da7a28263c0e3587b6804a9470b
yhli <liyunhai1016@hotmail.com>
2024-02-12 13:11:21 +0800

    Simplify get_option

--- physics/vasp/inpgen/inpgen.py
+++ physics/vasp/inpgen/inpgen.py
@@ -23,8 +23,12 @@ def get_option(prompt: str, options: Iterable[str]) -> str:
     :param options: available options
     :return: chosen option
     """
+    full_prompt = f"{prompt} ("
+    for s in options:
+        full_prompt += f"{s}/"
+    full_prompt = full_prompt[:-1] + ")"
     while True:
-        option = get_input(prompt)
+        option = get_input(full_prompt)
         if option in options:
             break
     return option
@@ -42,7 +46,7 @@ def get_macro() -> Dict[str, str]:
     macro = dict()
 
     # Get the type of job
-    macro[".job"] = get_option("Input type of job (opt/neb/bader)",
+    macro[".job"] = get_option("Input type of job",
                                options=("opt", "neb", "bader"))
 
     # Get specific job information
@@ -58,14 +62,14 @@ def get_macro() -> Dict[str, str]:
     macro["NCPU"] = get_input("Input number of cpu to use")
     macro["TIME"] = get_input("Input time limit (in hours)")
     macro["MEM"] = 2048  # maximum memory, temporarily fixed to 2048 MB
-    restart = get_option("Input whether to restart (yes/no)",
+    restart = get_option("Input whether to restart",
                          options=("yes", "no"))
     if restart == "yes":
         macro["ISTART"] = 1
     else:
         macro["ISTART"] = 0
     macro["RUN"] = get_input("Input number of run")
-    macro[".incar"] = get_option("Input whether to write INCAR (yes/no)",
+    macro[".incar"] = get_option("Input whether to write INCAR",
                                  options=("yes", "no"))
     return macro
 
